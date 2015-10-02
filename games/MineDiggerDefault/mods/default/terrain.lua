13165ea360191e40016b428d1d608f051940430a
Megaf <mmegaf@gmail.com>
2015-10-02 17:41:35 +0100

    Update terrain.lua

--- games/MineDiggerDefault/mods/default/terrain.lua
+++ games/MineDiggerDefault/mods/default/terrain.lua
@@ -1,23 +1,19 @@
 terrain = {}
 
-  -- Stone
-  minetest.register_node("default:stone", {
-	  description = "Stone",
-	  tiles ={"default_stone.png"},
-	  groups = {cracky=3},
-	  drop = 'default:stone',
-	  legacy_mineral = true,
-	  -- sounds = default.node_sound_stone_defaults(),
-  })
-  
-  -- Dirt
-  minetest.register_node("default:dirt", {
-	description = "Dirt",
-	tiles ={"default_grass.png",
-		tileable_vertical = false}},
-	groups = {crumbly=3, soil=1},
-	drop = 'default:dirt',
-	-- sounds = default.node_sound_dirt_defaults({
-		-- footstep = {name="default_grass_footstep", gain=0.4},
-	  }),
-  })
+-- Stone
+	minetest.register_node("default:stone", {
+		description = "Stone",
+		tiles ={"default_stone.png"},
+		groups = {cracky=3},
+		drop = 'default:stone',
+		legacy_mineral = true,
+	})
+
+-- Dirt
+	minetest.register_node("default:dirt", {
+		description = "Dirt",
+		tiles ={"default_grass.png",
+		tileable_vertical = false},
+		groups = {crumbly=3, soil=1},
+		drop = 'default:dirt',
+	})
