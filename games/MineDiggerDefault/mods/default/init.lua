818209fb022aabcf4869dcd98c726aeb7f023164
Megaf <mmegaf@gmail.com>
2015-10-02 17:20:56 +0100

    Update init.lua

--- games/MineDiggerDefault/mods/default/init.lua
+++ games/MineDiggerDefault/mods/default/init.lua
@@ -1,3 +1,6 @@
 default = {}
-
-
+  -- Load mapgen file.
+  dofile(minetest.get_modpath("default").."/mapgen.lua")
+  
+  -- Load basic terrain nodes file.
+  dofile(minetest.get_modpath("default").."/terrain.lua")
