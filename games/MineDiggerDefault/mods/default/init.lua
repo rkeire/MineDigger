2bf3b1d2567fb21a990579a44161f87f8f430a41
RegLnx <reglnx@outlook.com>
2015-11-30 15:20:52 +0000

    Complete update to come. Revoming everything. Rebase to the latest master branch of Minetest.

--- games/MineDiggerDefault/mods/default/init.lua
+++
@@ -1,6 +0,0 @@
-default = {}
-  -- Load mapgen file.
-  dofile(minetest.get_modpath("default").."/mapgen.lua")
-  
-  -- Load basic terrain nodes file.
-  dofile(minetest.get_modpath("default").."/terrain.lua")
